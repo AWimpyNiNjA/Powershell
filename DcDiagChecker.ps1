c436d5c71e30cc28797b0cf82d24c5000f40cc9a
AWimpyNiNjA <45632772+AWimpyNiNjA@users.noreply.github.com>
2020-07-28 10:27:50 -0400

    Rename dcdiag.ps1 to DcDiagChecker.ps1

--- DcDiagChecker.ps1
+++ DcDiagChecker.ps1
@@ -3,7 +3,7 @@
 $Path = "C:\admin\dcdiaglogs"
 
 #Date can be changed or reformatted.
-$Date = (Get-Date).ToString(“MM/dd/yyyy”).Replace(“/”,”-“)
+$Date = (Get-Date).ToString(â€œMM/dd/yyyyâ€).Replace(â€œ/â€,â€-â€œ)
 
 #Source can be changed to whichever source you want to use.
 $Source = "DCDIAG"
